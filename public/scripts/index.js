e4bb6dfefc2d826373240ae5d4532fbbcd5c8faa
LukezLima <lucas.dbz.lima@hotmail.com>
2020-12-27 01:27:06 -0300

    Altura let

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-let peso = 3;
-
-=======
-
 
 let IMC = 0;
 
 function calcularIMC(peso, altura) {
-    return IMC = peso / altura**2  
+    return IMC = peso / altura ** 2
 }
->>>>>>> 17ad107809a871b3e263a7449fb8991958460ba7
+
+let altura = 4;
